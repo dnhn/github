28b81266c97f413467f259b581fa318ac630588c
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2024-12-07 11:59:07 +0700

    Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  content: ["./src/app/**/*.{css,js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 } satisfies Config;
